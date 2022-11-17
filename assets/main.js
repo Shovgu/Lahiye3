f6d9d87c2fe6b5418abb336c25de3ac9ecb9c585
Shovgu <mmmdov208@gmail.com>
2022-11-17 12:52:44 +0400

    Third Day

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,32 @@
-function changeOfCurrencies(e){
-    const firstInput=document.getElementsByName("first-input")[0];
-    const secondInput=document.getElementsByName("second-input")[0];
-    const currencyChange=document.querySelector(".currency");
-    const currencyChange1=document.querySelector(".currency1");
-    
-    if(e.target.className=="currency" || e.target.className=="currency1"){
-        const changes=[...e.target.parentElement.children];
-        changes.forEach(item=>{
-            if(item.classList.contains("change")){
-                item.classList.remove("change");
-            }
-        })
-        e.target.classList.add("change");
-    };
-    if(rubl.classList.contains("change") && rubl1.classList.contains("change")){
-        secondInput.value=Number(firstInput.value);
-    };
-    if(currencyChange.classList.contains("change") && currencyChange1.classList.contains("change")){
-        fetch(`https://api.exchangerate.host/latest?base=${currencyChange.innerText}&symbols=${currencyChange1.innerText}`)
-        .then(r=>r.json())
-        .then((data)=>{
-            // secondInput.value=Number(firstInput.value)*data.rates.currencyChange1.innerText
-            console.log(currencyChange.innerHTML)
-        })
-    };
-};
+const firstInput=document.getElementsByName("first-input")[0];
+const secondInput=document.getElementsByName("second-input")[0];
+const one=document.getElementsByName("one");
+const two=document.getElementsByName("two");
+
+let first;
+let second;
+
+firstInput.addEventListener("keypress",()=>{
+
+    one.forEach(item=>{
+        if(item.classList.contains("change")){
+            console.log(item.innerText)
+            first = item.innerText
+        }
+       
+    })
+    two.forEach(item=>{
+        if(item.classList.contains("change")){
+            console.log(item.innerText)
+            second= item.innerText
+        }
+    })
+    fetch(`https://api.exchangerate.host/latest?base=${first}&symbols=${second}`)
+             .then(r=>r.json())
+             .then((data)=>{
+                 secondInput.value=Number(firstInput.value)*data.rates[second]
+             })
+})
 const rubl=document.getElementById("rubl");
 const dollar=document.getElementById("dollar");
 const euro=document.getElementById("euro");
@@ -44,3 +46,50 @@ rubl1.addEventListener("click",changeOfCurrencies);
 dollar1.addEventListener("click",changeOfCurrencies);
 euro1.addEventListener("click",changeOfCurrencies);
 fs1.addEventListener("click",changeOfCurrencies);
+
+
+
+function changeOfCurrencies(e){
+    // const firstInput=document.getElementsByName("first-input")[0];
+    // console.log(firstInput)
+    // const secondInput=document.getElementsByName("second-input")[0];
+    // const currencyChange=document.querySelector(".currency");
+    // const currencyChange1=document.querySelector(".currency1");
+    // const one=document.getElementsByName("one");
+    // const two=document.getElementsByName("two");
+    
+    if(e.target.className=="currency" || e.target.className=="currency1"){
+        const changes=[...e.target.parentElement.children];
+        changes.forEach(item=>{
+            if(item.classList.contains("change")){
+                item.classList.remove("change");
+            }
+        })
+        e.target.classList.add("change");
+    };
+   
+
+
+    // [...one].forEach(item=>{
+    //     if(item.classList.contains("change") && .classList.contains("change")){
+    //         fetch(`https://api.exchangerate.host/latest?base=${one.innerText}&symbols=${two.innerText}`)
+    //         .then(r=>r.json())
+    //         .then((data)=>{
+    //             secondInput.value=Number(firstInput.value)*data.rates.two.innerText
+    //         })
+    //     };
+    // })
+
+    // if(one.classList.contains("change") && two.classList.contains("change")){
+    //     fetch(`https://api.exchangerate.host/latest?base=${one.innerText}&symbols=${two.innerText}`)
+    //     .then(r=>r.json())
+    //     .then((data)=>{
+    //         secondInput.value=Number(firstInput.value)*data.rates.two.innerText
+    //     })
+    // };
+    // if(rubl.classList.contains("change") && rubl1.classList.contains("change")){
+    //     secondInput.value=Number(firstInput.value);
+    //     exchangeRate.innerText=`1 RUB=1 RUB`
+    // };
+
+};
